9a0ceed94ac16f5cc3a5fcdcd2e02bd56c4689d3
17tangn58 <17tangn58@millshighsch.org>
2016-08-29 09:42:40 -0700

    Finished Pikachu and Hello World

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author APCS2 Nathan Tang
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, my name is Nathan");
+	}
+
+}

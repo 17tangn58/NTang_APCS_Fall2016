d3e62438573b00a77a520e522d8584c7a5f92836
17tangn58 <17tangn58@millshighsch.org>
2016-09-01 09:50:36 -0700

    Did more work on Calculate Library
    need to fix strings(everything after discriminant

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -9,8 +9,9 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		
-		Calculate.square(5);
-		
+		int num1 = 360;
+		int num2 = 5;
+		int num3 = 5;
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
 	}
 }
---
+++ TempConvert/src/TempConvert.java
@@ -0,0 +1,12 @@
+
+public class TempConvert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double F = -32;
+		double C = (5/9)*(F-32);
+		System.out.println("The temperature in Celcius is " + C);
+		
+	}
+
+}

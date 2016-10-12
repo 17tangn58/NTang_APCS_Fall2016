4839a741c4b42bc88cbcd0ca3647a02fe6143dcb
17tangn58 <17tangn58@millshighsch.org>
2016-10-11 21:51:41 -0700

    Magpie 2 and start to Magpie 3

--- MagPie2/src/Magpie2.java
+++ MagPie2/src/Magpie2.java
@@ -20,7 +20,19 @@ public class Magpie2 {
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
-		} else {
+		}else if (statement.indexOf("Latham")>=0
+				||statement.indexOf("Dreyer")>=0
+				||statement.indexOf("Paul")>=0
+				||statement.indexOf("Dwyer")>=0
+				||statement.indexOf("Louie")>=0
+				||statement.indexOf("Headley")>=0){
+			response = "He or she sounds like a good teacher.";
+		}else if (statement.indexOf("hi")>=0||statement.indexOf("hello")>=0){
+			response = "How are you?";
+		}else if(statement.indexOf("election")>=0||statement.indexOf("Hillary")>=0||statement.indexOf("Trump")>=0){
+			response = "I don't really follow politics.";
+		
+		}else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -31,7 +43,7 @@ public class Magpie2 {
 	 * returns a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -44,8 +56,11 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+		    response="Okay.";
+		} else if (whichResponse == 5) {
+			response="What else do you have to say?";
 		}
-
 		return response;
 	}
 }
